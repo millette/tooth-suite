9ec197bcb9d20760c6e047a334feaf84aa6c112f
Robin Millette <robin@millette.info>
2019-04-29 15:20:39 -0400

    hmmm

--- components/button.js
+++ components/button.js
@@ -1,14 +1,19 @@
-export default ({ children }) => (
+// const React = require('react')
+
+// export default ({ children }) => (
+const Button = ({ children }) => (
   <button
     style={{
-      borderRadius: '3px',
-      border: '1px solid black',
-      color: 'black',
-      padding: '0.5em 1em',
-      cursor: 'pointer',
-      fontSize: '1.1em'
+      borderRadius: "3px",
+      border: "1px solid black",
+      color: "black",
+      padding: "0.5em 1em",
+      cursor: "pointer",
+      fontSize: "1.1em",
     }}
   >
     {children}
   </button>
 )
+
+module.exports = button
