772773c3ac8dfa1f04917bd09dd796cd78421b9e
Robin Millette <robin@millette.info>
2019-04-29 14:53:18 -0400

    Simplify config.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
+  // extension: /\.mdx?$/,
+  /*
+  options: {
+    mdPlugins
+  }
+  */
 })
+
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx"],
 })
