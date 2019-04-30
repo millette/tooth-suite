378b783abfd64607df4a5a69d058e6ba6355467e
Robin Millette <robin@millette.info>
2019-04-30 08:25:42 -0400

    Second button, to test mdx provider override.

---
+++ components/button2.js
@@ -0,0 +1,14 @@
+export default ({ children }) => (
+  <button
+    style={{
+      borderRadius: "3px",
+      border: "3px solid yellow",
+      color: "blue",
+      padding: "0.5em 1em",
+      cursor: "pointer",
+      fontSize: "2.1em",
+    }}
+  >
+    {children}
+  </button>
+)
