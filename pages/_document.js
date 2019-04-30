70224f94d6a55ee7346d0278e7da2400ea401b79
Robin Millette <robin@millette.info>
2019-04-30 10:58:01 -0400

    Style with water.css.

---
+++ pages/_document.js
@@ -0,0 +1,40 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            title="Light"
+            href="https://cdn.jsdelivr.net/gh/kognise/water.css@1.4.0/dist/light.min.css"
+            integrity="sha256-3AWyWETbVodg3oCrgu2/JmO4Q5JKGo+G7Ubf66bClh4="
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="alternate stylesheet"
+            title="Dark"
+            href="https://cdn.jsdelivr.net/gh/kognise/water.css@1.4.0/dist/dark.min.css"
+            integrity="sha256-a4zpugsKZqeQuXHZob2UNRXchaLIJRFdXCdi11HgAKY="
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <footer>
+            <p>Fee, Fii, Foo</p>
+          </footer>
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
