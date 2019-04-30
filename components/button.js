180b684c5889a2cdf44f06431abd668e115e6101
Robin Millette <robin@millette.info>
2019-04-30 10:58:23 -0400

    Remove button color, let water.css handle it.

--- components/button.js
+++ components/button.js
@@ -3,7 +3,6 @@ export default ({ children }) => (
     style={{
       borderRadius: "3px",
       border: "1px solid black",
-      color: "black",
       padding: "0.5em 1em",
       cursor: "pointer",
       fontSize: "1.1em",
