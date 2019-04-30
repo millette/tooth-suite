895094fa5ba95b0efae7f7a63ef9a04cc86349f8
Robin Millette <robin@millette.info>
2019-04-30 13:14:22 -0400

    Use remark plugins.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 "use strict"
 
 // npm
-const withMDX = require("@next/mdx")()
+// const collapse = require('remark-collapse')
+// const normalize = require('remark-normalize-headings')
+// const toc = require('remark-toc')
+const slug = require("remark-slug")
+const autolink = require("remark-autolink-headings")
+const sectionize = require("remark-sectionize")
+const withMDX = require("@next/mdx")({
+  options: {
+    remarkPlugins: [
+      // normalize,
+      slug,
+      autolink,
+      // toc,
+      sectionize,
+      // [collapse, { test: "Header level 2" }],
+    ],
+  },
+})
 
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx"],
