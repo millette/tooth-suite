cf8cad5a24f37f35647fbe0fcb2908d176932925
Robin Millette <robin@millette.info>
2019-05-01 08:43:58 -0400

    Add dentist component.

---
+++ components/dentist.js
@@ -0,0 +1,34 @@
+const style = {
+  margin: "1.5rem 0",
+  borderBottom: "thin dashed",
+  display: "flex",
+  flexFlow: "row wrap",
+}
+
+export default ({
+  phone,
+  types,
+  name,
+  vicinity,
+  rating,
+  user_ratings_total,
+}) => (
+  <section style={style}>
+    <div style={{ flex: 1 }}>
+      <h3>{name}</h3>
+      <h4>{phone}</h4>
+    </div>
+    <dl style={{ flex: 1 }}>
+      <dt>Rating</dt>
+      <dd>
+        {rating} (of {user_ratings_total})
+      </dd>
+
+      <dt>Address</dt>
+      <dd>{vicinity}</dd>
+
+      <dt>Types</dt>
+      <dd>{types.join(", ")}</dd>
+    </dl>
+  </section>
+)
