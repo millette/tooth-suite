22b1450103556acf9a798c44058869d2d0bfcb2b
Robin Millette <robin@millette.info>
2019-05-03 17:09:53 -0400

    Geocode postal code (google api).

--- components/dentists.js
+++ components/dentists.js
@@ -7,10 +7,16 @@ import Dentist from "./dentist"
 
 const num = () => Math.max(1000, Math.floor(Math.random() * 10000))
 
+/*
 const fix = (x) => ({
   ...x,
   phone: `514-555-${num()}`,
 })
+*/
+
+// const displayZip = (zip) => [zip.slice(0, 3), zip.slice(3)].join(' ')
+
+const language = "fr-CA"
 
 const byRating = (a, b) => {
   if (a.rating > b.rating) return 1
@@ -19,15 +25,25 @@ const byRating = (a, b) => {
   if (a.user_ratings_total < b.user_ratings_total) return -1
 }
 
-// <p>And one more thing: <b>{process.env.GOOGLE_MAPS}</b>.</p>
+const zipRE1 = /[^A-Z0-9]/g
+const zipRE2 = /H[0-57-9]([A-Z][0-9]){2}/ // Montréal begins with H but never H6
+// const zipRE2 = /([A-Z][0-9]){3}/ // Canada
+
+const normalizeZip = (zip) => {
+  const x = zip.toUpperCase().replace(zipRE1, "")
+  return zipRE2.test(x) ? x : ""
+}
 
 export default ({ step = 0.1 }) => {
-  const [dentists, setDentists] = useState()
+  // const [dentists, setDentists] = useState()
   const [selectedDentists, setSelectedDentists] = useState([])
-  const [near, setNear] = useState("")
+  // const [near, setNear] = useState("")
+  const [zip, setZip] = useState()
+  const [coords, setCoords] = useState()
   const [min, setMin] = useState(5)
   const [dir, setDir] = useState(-step)
 
+  /*
   const minRating = ({ rating }) => rating >= min
 
   useEffect(() => {
@@ -61,6 +77,26 @@ export default ({ step = 0.1 }) => {
         .reverse()
     )
   }, [dentists, min])
+  */
+
+  useEffect(() => {
+    if (!zip) return
+    get(["zip", language, zip].join(":"))
+      .then(
+        (val) =>
+          val ||
+          fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?language=${language}&region=ca&components=postal_code:${zip}&key=${
+              process.env.GOOGLE_MAPS
+            }`
+          )
+            .then((res) => res.json())
+            .then((x) => {
+              return set(["zip", language, zip].join(":"), x).then(() => x)
+            })
+      )
+      .then(setCoords)
+  }, [zip])
 
   const click = () => {
     const n = min + 2 * dir
@@ -70,8 +106,9 @@ export default ({ step = 0.1 }) => {
 
   const submit = (ev) => {
     ev.preventDefault()
-    const near = new FormData(ev.target).get("near")
-    setNear(near)
+    const zip = normalizeZip(new FormData(ev.target).get("near"))
+    // setNear(near)
+    setZip(zip)
   }
 
   return (
@@ -91,29 +128,25 @@ export default ({ step = 0.1 }) => {
         <form onSubmit={submit}>
           <label>
             Search (enter to submit){" "}
-            <input
-              name="near"
-              defaultValue={near}
-              placeholder="H2K 4B2"
-              type="text"
-            />
+            <input name="near" placeholder="H2K 4B2" type="text" />
           </label>
-          {near && (
+          {zip && coords && coords.results && coords.results[0] && (
             <p>
-              Near: <b>{near}</b>.
+              Near <b>{coords.results[0].formatted_address}</b>.<br />
+              {coords.results[0].place_id}
+              <br />
+              {JSON.stringify(coords.results[0].geometry.location)}
             </p>
           )}
         </form>
       </div>
-      <div>
-        {selectedDentists.length ? (
-          selectedDentists.map((dentist) => (
-            <Dentist key={dentist.place_id} {...dentist} />
-          ))
-        ) : (
-          <p>Enter a postal code to search near.</p>
-        )}
-      </div>
+      {selectedDentists.length ? (
+        selectedDentists.map((dentist) => (
+          <Dentist key={dentist.place_id} {...dentist} />
+        ))
+      ) : (
+        <p>Enter a postal code to search near.</p>
+      )}
     </>
   )
 }
