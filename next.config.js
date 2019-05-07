e49ca6865d3484bef3e4b192133386aa08abc540
Robin Millette <robin@millette.info>
2019-05-07 14:49:40 -0400

    Use webpack-bundle-analyzer support lib.

--- next.config.js
+++ next.config.js
@@ -15,17 +15,38 @@ const withMDX = require("@next/mdx")({
   },
 })
 
+// self
+// Taken from https://github.com/zeit/next.js/tree/canary/packages/next-bundle-analyzer
+// with updated webpack-bundle-analyzer dep
+const withBundleAnalyzer = require("./lib/bundan")({
+  enabled: process.env.ANALYZE === "true",
+})
+
 const path = join(__dirname, ".env")
 
-module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "mdx"],
-  webpack: (config) => {
-    const dots = new Dotenv({ path })
-    if (config.plugins) {
-      config.plugins.push(dots)
-    } else {
-      config.plugins = [dots]
-    }
-    return config
-  },
-})
+module.exports = withBundleAnalyzer(
+  withMDX({
+    pageExtensions: ["js", "jsx", "mdx"],
+    analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: "static",
+        reportFilename: "../bundles/server.html",
+      },
+      browser: {
+        analyzerMode: "static",
+        reportFilename: "../bundles/client.html",
+      },
+    },
+    webpack: (config) => {
+      const dots = new Dotenv({ path })
+      if (config.plugins) {
+        config.plugins.push(dots)
+      } else {
+        config.plugins = [dots]
+      }
+      return config
+    },
+  })
+)
