bd5c6820885fe23d2dac1c7af4d15f72d4b6349d
Robin Millette <robin@millette.info>
2019-05-16 13:25:38 -0400

    Handle all questions.

--- components/details.js
+++ components/details.js
@@ -5,17 +5,31 @@ import { useState, useEffect } from "react"
 import { jsonStoreUrl } from "../utils/json-store.js"
 
 export default ({ place_id }) => {
+  const [questions, setQuestions] = useState()
   const [details, setDetails] = useState()
   const [message, setMessage] = useState()
 
   useEffect(() => {
-    fetch(jsonStoreUrl(["details", place_id]))
+    fetch(jsonStoreUrl("questions"))
       .then((res) => res.json())
       .then((json) => {
-        const { result, ok } = json
-        if (ok) return setDetails(result || {})
-        setMessage(`error: ${JSON.stringify(json)}`)
+        if (json.ok) return json.result || []
+        throw new Error(JSON.stringify(json))
       })
+      .then((qqq) => {
+        fetch(jsonStoreUrl(["details", place_id]))
+          .then((res) => res.json())
+          .then((json) => {
+            const { result, ok } = json
+            if (ok) {
+              setQuestions(qqq)
+              setDetails(result || {})
+              return
+            }
+            throw new Error(JSON.stringify(json))
+          })
+      })
+      .catch(setMessage)
   }, [])
 
   const submit = (ev) => {
@@ -23,11 +37,12 @@ export default ({ place_id }) => {
     const method = "PUT"
     const action = jsonStoreUrl(["details", place_id])
     const headers = { "content-type": "application/json" }
-    const fd = new FormData(ev.target)
-    const q1 = fd.get("q1")
-    const q2 = fd.get("q2")
 
-    const deets = { q1, q2 }
+    const deets = {}
+    for (const [k, v] of new FormData(ev.target).entries()) {
+      if (v) deets[k] = v
+    }
+
     const body = JSON.stringify(deets)
 
     fetch(action, {
@@ -48,21 +63,18 @@ export default ({ place_id }) => {
       <h2>
         Details <small>({place_id})</small>
       </h2>
-
+      {message && <p>{message}</p>}
       {details && (
         <form onSubmit={submit}>
-          <label>
-            Q1...
-            <textarea name="q1" defaultValue={details.q1} />
-          </label>
-          <label>
-            Q2...
-            <textarea name="q2" defaultValue={details.q2} />
-          </label>
+          {questions.map(({ text, key }) => (
+            <label key={key}>
+              {key.toUpperCase()}: {text}
+              <textarea name={key} defaultValue={details[key]} />
+            </label>
+          ))}
           <button>Submit</button>
         </form>
       )}
-      {details && <pre>DETAILS: {JSON.stringify(details, null, "  ")}</pre>}
       {message && <p>{message}</p>}
     </div>
   )
