33e9376800f243556fcd243e31070f92f99c26ec
Robin Millette <robin@millette.info>
2019-05-16 14:43:41 -0400

    Clean up.

--- components/title.js
+++ components/title.js
@@ -4,8 +4,6 @@ import Link from "next/link"
 // self
 import iii from "../static/tooth.png"
 
-// [Tooth Suite!](/) [![Broken tooth](/static/tooth.png)](/)
-
 export default () => (
   <h1>
     <Link href="/" prefetch>
